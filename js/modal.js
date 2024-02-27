e835570495f4d9185acc69bdd739436ead1428e9
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-02-27 09:17:43 +0200

    add no scroll to body when modal is open

--- js/modal.js
+++ js/modal.js
@@ -4,10 +4,26 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  refs.openModalBtn.addEventListener("click", () => {
+    toggleModal(true);
+  });
+
+  refs.closeModalBtn.addEventListener("click", () => {
+    toggleModal(false);
+  });
+
+  function toggleModal(open) {
+    refs.modal.classList.toggle("is-hidden", !open);
+
+    // Get the body element
+    const body = document.body;
+
+    // Toggle the class on the body to block or allow scrolling
+    if (open) {
+      body.classList.add("modal-open");
+    } else {
+      body.classList.remove("modal-open");
+    }
   }
 })();
